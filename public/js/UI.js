de3bdabbb231bb7f4b106dc70809c1459a915f8a
ashwin6113 <ashwinjohn611@gmail.com>
2023-07-24 08:16:21 +0530

    changes in the file

--- public/js/UI.js
+++ public/js/UI.js
@@ -1,6 +1,9 @@
 var uploadButton = document.getElementById("uploadButton");
 var compressButton = document.getElementById("compressButton");
 var blogButton = document.getElementById("blogButton");
+var blogArrow = document.getElementById("blogArrow");
+var compressArrow = document.getElementById("compressArrow");
+
 
 uploadButton.addEventListener("click",() => {
     uploadButton.style.backgroundColor = "#60de29";
@@ -9,9 +12,13 @@ uploadButton.addEventListener("click",() => {
 compressButton.addEventListener("click",() => {
     compressButton.style.backgroundColor = "#60de29";
     blogButton.style.backgroundColor = "#fff";
+    compressArrow.style.display = "none";
+    blogArrow.style.display = "inline-block";
 });
 
 blogButton.addEventListener("click",() => {
     blogButton.style.backgroundColor = "#60de29";
     compressButton.style.backgroundColor = "#fff";
+    compressArrow.style.display = "inline-block";
+    blogArrow.style.display = "none";
 });
