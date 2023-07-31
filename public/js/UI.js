d8f5cdc0050c9f8eacb27ae49de66cb8c5a4e5e4
AlenJoseph05 <alenjoseph03@gmail.com>
2023-07-31 20:25:10 +0530

    Made navbar a seperate component

--- public/js/UI.js
+++ public/js/UI.js
@@ -8,13 +8,3 @@ var compressArrow = document.getElementById("compressArrow");
 uploadButton.addEventListener("click",() => {
     uploadButton.style.backgroundColor = "#60de29";
 });
-
-compressButton.addEventListener("click",() => {
-    blogArrow.style.display = "inline-block";
-    compressArrow.style.display = "none";
-});
-
-blogButton.addEventListener("click",() => {
-       compressArrow.style.display = "inline-block";
-    blogArrow.style.display = "none";
-});
