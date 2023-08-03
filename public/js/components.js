5c2dbab081401bfbab59f41d01e948c2bdccb53e
AlenJoseph05 <alenjoseph03@gmail.com>
2023-08-04 00:47:35 +0530

    Beautify code

--- public/js/components.js
+++ public/js/components.js
@@ -1,25 +1,25 @@
 // navigation.js
 document.addEventListener('DOMContentLoaded', function () {
-    // Fetch the navigation.html file
-    fetch('../../components/navbar.html')   
-      .then(function (response) {
-        return response.text();
-      })
-      .then(function (html) {
-        // Insert the navigation bar into the header element of each page
-        document.getElementById('navbar').innerHTML = html;
-      })
-      .catch(function (error) {
-        console.error('Error fetching navigation bar:', error);
-      });
-  });
-  
+  // Fetch the navigation.html file
+  fetch('../../components/navbar.html')
+    .then(function (response) {
+      return response.text();
+    })
+    .then(function (html) {
+      // Insert the navigation bar into the header element of each page
+      document.getElementById('navbar').innerHTML = html;
+    })
+    .catch(function (error) {
+      console.error('Error fetching navigation bar:', error);
+    });
+});
 
 
-  // navigation.js
+
+// navigation.js
 document.addEventListener('DOMContentLoaded', function () {
   // Fetch the navigation.html file
-  fetch('../../components/footer.html')   
+  fetch('../../components/footer.html')
     .then(function (response) {
       return response.text();
     })
